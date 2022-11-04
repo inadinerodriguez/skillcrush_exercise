2be934b28abe78b00ded5b5e555512f2da2bd81c
Nadine Rodriguez <inadinerod@gmail.com>
2022-11-04 11:28:53 -0400

    Add lesson 9 challenge 3

---
+++ js_l9_c3.js
@@ -0,0 +1,46 @@
+// Find the min/max
+// Link https://l3dc9y.csb.app/
+
+const button = document.querySelector(".show-results");
+const results = document.querySelector(".results");
+const expensesList = document.querySelector(".expenses");
+
+const expenses = [
+  9.99,
+  25.8,
+  144.39,
+  12.98,
+  4.99,
+  38.75,
+  14.5,
+  99.99,
+  75.4,
+  4.75,
+  62.88
+];
+
+// Display your expenses in a list
+for (let exp of expenses) {
+  let li = document.createElement("li");
+  li.innerText = `$ ${exp}`;
+  expensesList.append(li);
+}
+
+// Write showMinMax function here
+const showMinMax = function (expenses) {
+  const min = Math.min(...expenses);
+  const max = Math.max(...expenses);
+  let minList = document.createElement("li");
+  minList.innerText = `Min: $ ${min}`;
+  let maxList = document.createElement("li");
+  maxList.innerText = `Max: $ ${max}`;
+  results.append(minList);
+  results.append(maxList);
+  results.classList.remove("hide");
+};
+
+// Write click event listener for 'button' and call the showMinMax function
+button.addEventListener("click", function () {
+  showMinMax(expenses);
+  button.disabled = true;
+});
