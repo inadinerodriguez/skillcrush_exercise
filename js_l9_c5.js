c2365ed1b155968ff06657cc5c2bd0586d2a1a8b
Nadine Rodriguez <inadinerod@gmail.com>
2022-11-04 11:57:19 -0400

    Add lesson 9 challenge 5

---
+++ js_l9_c5.js
@@ -0,0 +1,36 @@
+// Todo list https://h76lxk.csb.app/
+
+const todos = [
+    "Walk dog",
+    "Do laundry",
+    "Call mom",
+    "Finish paper",
+    "Code a small project",
+    "Read docs",
+    "Grab food",
+    "Go to the post office"
+  ];
+  
+  const button = document.querySelector(".show-todos");
+  const todoList = document.querySelector(".todos");
+  
+  const showTodos = function () {
+    todos.forEach(function (todo) {
+      let li = document.createElement("li");
+      li.innerHTML = todo;
+      todoList.append(li);
+    });
+    todoList.classList.remove("hide");
+    button.innerText = "Hide To-Dos";
+  };
+  
+  button.addEventListener("click", function () {
+    if (todoList.classList.contains("hide")) {
+      showTodos();
+    } else {
+      button.innerText = "Show me!";
+      todoList.classList.add("hide");
+      todoList.innerHTML = "";
+    }
+  });
+  
